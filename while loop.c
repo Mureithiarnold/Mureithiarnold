51b7470c88503995e64de065bd9042ce17692d47
Mureithiarnold <101640546+Mureithiarnold@users.noreply.github.com>
2022-04-26 15:11:35 +0300

    Add files via upload

---
+++ while loop.c
@@ -0,0 +1,29 @@
+
+
+#include<stdio.h>
+int main ()
+{
+ /* local variable initialization */   int n= 1,times=5;
+ /* do loops execution */ do
+ {
+     printf("c do while loops: %d\n", n);
+     n = n + 1;
+ }
+ while (n <= times);
+
+ return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
